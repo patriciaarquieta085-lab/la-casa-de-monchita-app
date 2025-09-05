8a1555c5f7125bdbab223a87630653c5932bbd01
patriciaarquieta085-lab <patriciaarquieta085@gmail.com>
2025-09-05 00:32:43 -0600

    Crear el componente Orders

---
+++ src/pages/Orders.tsx
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function Orders() {
+  return (
+    <section>
+      <h2 className="text-2xl font-bold mb-4">Tus pedidos</h2>
+      <p>Listado de tus pedidos realizados.</p>
+    </section>
+  )
+}
