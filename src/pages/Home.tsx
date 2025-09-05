9b4aca603c02533abd39d4b57506195a24aeb1e6
patriciaarquieta085-lab <patriciaarquieta085@gmail.com>
2025-09-05 00:33:19 -0600

    Crear el archivo Home.tsx en src/pages

---
+++ src/pages/Home.tsx
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function Home() {
+  return (
+    <section>
+      <h2 className="text-3xl font-bold mb-6">Bienvenido a La Casa de Monchita</h2>
+      <p className="mb-4">¡Haz tu pedido de comida para entrega!</p>
+    </section>
+  )
+}
