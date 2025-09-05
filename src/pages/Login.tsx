1e55a4586b729abf35cc6045382d30539ec62cdb
patriciaarquieta085-lab <patriciaarquieta085@gmail.com>
2025-09-05 00:33:05 -0600

    Crear el componente Login

---
+++ src/pages/Login.tsx
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function Login() {
+  return (
+    <section>
+      <h2 className="text-2xl font-bold mb-4">Login</h2>
+      <p>Página de acceso de usuario.</p>
+    </section>
+  )
+}
